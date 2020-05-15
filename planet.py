a27ae23386bbe1179b92a87c35abeed88189d925
Jerome <jung@ikf.uni-frankfurt.de>
2020-05-15 13:48:58 +0200

    Added setters, moved G

--- planet.py
+++ planet.py
@@ -1,34 +1,14 @@
 import numpy as np
 
 class Planet:
-    # def next_timestep(self, delta_t, planets):
-    #     self.new_position = self.position + self.velocity * delta_t
-
-    #     a = np.array([0., 0., 0.])
-    #     for planet in planets:
-    #         if planet.name == self.name:
-    #             continue
-    #         rel_position = planet.position - self.position
-    #         denominator = np.linalg.norm(rel_position, ord = 2)
-    #         a += self.G * planet.mass * rel_position / denominator
-        
-    #     self.new_velocity = self.velocity + a * delta_t
-
-    #     # print("----------------")
-    #     # print(self.name)
-    #     # print("v_0: {}, v_1: {}".format(self.velocity, self.new_velocity))
-    #     # print("x_0: {}, x_1: {}".format(self.position, self.new_position))
-
-    # def update_position_and_velocity(self):
-    #     self.new_position, self.position = self.position, self.new_position
-    #     self.new_velocity, self.velocity = self.velocity, self.new_velocity
-
+    
     def __init__(self, mass, position, velocity, name):
-        self.G = 6.67430 * 1E-11 # m^3 / kg * s^2
+#        self.G = 6.67430 * 1E-11 # m^3 / kg * s^2 sollte eher groesse der Sim sein
         self.mass = mass
-        self.set_position(position)
-        self.set_velocity(velocity)
+        self.position = position
+        self.velocity = velocity
         self.name = name
+ #       self.radius = R
 
     @property
     def mass(self):
@@ -41,11 +21,31 @@ class Planet:
         else:
             self.__mass = value
 
-    def set_position(self, vector):
-        self.position = vector
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, vector):
+        self.__position = vector
     
-    def set_velocity(self, vector):
-        self.velocity = vector
+
+    @property
+    def velocity(self):
+        return self.__velocity
+
+    @velocity.setter
+    def velocity(self, vector):
+        self.__velocity = vector
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, name):
+        self.__name = name
 
     def __str__(self):
         return "name: " + str(self.name) + "\nmass: " + str(self.mass) + "\nposition: " + str(self.position) + "\nvelocity: " + str(self.velocity)
