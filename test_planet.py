5538fba0f5c79c1c91a1ae5056f2fc4077986d44
Jerome <jung@ikf.uni-frankfurt.de>
2020-05-15 13:50:13 +0200

    Added test cases for planet setters

--- test_planet.py
+++ test_planet.py
@@ -10,7 +10,7 @@ def init_planet():
     return planet.Planet(mass, position1, velocity1, "Finera")
 
 def test_mass_setter(init_planet):
-    planet1   = init_planet
+    planet1 = init_planet
 
     new_mass = 100
     planet1.mass = new_mass
@@ -18,9 +18,43 @@ def test_mass_setter(init_planet):
     assert(planet1.mass) == new_mass
 
 def test_mass_setter_negative_mass(init_planet):
-    planet1   = init_planet
+    planet1 = init_planet
 
     new_mass = -100
     planet1.mass = new_mass
 
     assert(planet1.mass) == 0
+
+def test_position_setter(init_planet):
+    planet1 = init_planet
+
+    position2 = np.array([1., 0., 0.])
+    planet1.position = position2
+
+    assert (planet1.position == position2).all()
+
+def test_position_setter_different_shape(init_planet):
+    planet1 = init_planet
+
+    position2 = np.array([1., 0., 0.])
+    planet1.position = position2
+
+    assert np.array_equal(planet1.position,position2)
+    assert np.array_equiv(planet1.position,position2)
+    assert np.allclose(planet1.position,position2)
+
+def test_velocity_setter(init_planet):
+    planet1 = init_planet
+
+    velocity2 = np.array([1., 0., 0.])
+    planet1.velocity = velocity2
+
+    assert (planet1.velocity == velocity2).all() 
+
+def test_name_setter(init_planet):
+    planet1 = init_planet
+
+    name = "padawan"
+    planet1.name = name
+
+    assert(planet1.name) == name
